022248823c938efe18819bfb3126232d48279548
Panos Bozas <fbozas@hotmail.com>
2019-04-21 20:06:42 +0300

    Add project files.

---
+++ ObjectBuilding/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectBuilding
+{
+    class Customer
+    {
+        // Step 2 Now we have to create our properties..
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Reliable { get; set; }
+        public float Weight { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        // Step 3.A Now we have to write our default constructor. This constructor is already used 
+        // even though there is no need to write it. We write it now because we also want to use both default
+        // and custom constructor. If we delete the default constructor customer1 and customer2 object
+        // will shout "Hey I want my parameters inside my paretheses"
+
+        public Customer() // <-- This is the default constructor
+        {
+                
+        }
+
+        // Step 3.B Now we have to create our custom constructor in order to create an object in one line
+        // What if our properties didn't start with capital letter? see second version
+        public Customer(string firstName, string lastName, bool reliable, float weight, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Reliable = reliable;
+            Weight = weight;
+            DateOfBirth = dateOfBirth;
+        }
+
+        // Step 4 now we have to create ToString() method in order to print a whole object like this:
+        // Console.WriteLine(customer1), not like this Console.WriteLine(customer1.FirstName)
+        // Without ToString() method we would need too many Console.WriteLine() methods to print each property
+        public override string ToString()
+        {
+            return "Customer's name is: " + FirstName + " " + LastName + " his/her reliability is: " + Reliable + " his/her weight is: " + Weight + " and his/her date of birth is on: " + DateOfBirth; 
+        }
+    }
+}
---
+++ ObjectBuilding/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// How we can build an object? Let's build an object for a customer with three different ways
+namespace ObjectBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: The first thing we have to do is to create a customer class
+            // So click on ObjectBuilding project on your solution explorer,
+            // press right click and click "add class". Go to customer class now for the 2nd step 
+
+            // Now let's see how we can make objects this is the 1st way by using default constructor
+            Customer customer1 = new Customer();
+            customer1.FirstName = "Nick";
+            customer1.LastName = "Giannou";
+            customer1.Reliable = true;
+            customer1.Weight = 72.3f;
+            customer1.DateOfBirth = new DateTime(1990,2,3);
+
+            //This is the 2nd way by using default constructor
+            // (Don't forget commas between the properties)
+            Customer customer2 = new Customer()
+            {
+                FirstName = "Giannis",
+                LastName = "Eleftheratos",
+                Reliable = true,
+                Weight = 68.6f,
+                DateOfBirth = new DateTime(1991, 3, 7)
+            };
+
+            // This is the 3d way (and the fastest ;) ) by using the constructor we created
+            Customer customer3 = new Customer("George", "Papadopoulos", false, 66.9f, new DateTime(1993, 5, 2));
+
+            Console.WriteLine(customer1);
+            Console.WriteLine(customer2);
+            Console.WriteLine(customer3);
+        }
+    }
+}
